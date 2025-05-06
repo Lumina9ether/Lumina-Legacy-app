352520bf402483c5778be5cf108cd71275735c09
Lumina9ether <highvibrationbeings@gmail.com>
2025-05-06 00:53:33 -0400

    stars.js

--- static/stars.js
+++ static/stars.js
@@ -1,47 +1,41 @@
-const canvas = document.getElementById('stars');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("stars");
+const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 let stars = [];
 
-for (let i = 0; i < 200; i++) {
-    stars.push({
-        x: Math.random() * canvas.width,
-        y: Math.random() * canvas.height,
-        radius: Math.random() * 1.5,
-        velocity: Math.random() * 0.5 + 0.2
-    });
+for (let i = 0; i < 100; i++) {
+  stars.push({
+    x: Math.random() * canvas.width,
+    y: Math.random() * canvas.height,
+    r: Math.random() * 1.5,
+    d: Math.random() * 0.5
+  });
 }
 
 function drawStars() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = 'white';
-
-    stars.forEach(star => {
-        ctx.beginPath();
-        ctx.arc(star.x, star.y, star.radius, 0, Math.PI * 2);
-        ctx.fill();
-    });
-
-    updateStars();
-    requestAnimationFrame(drawStars);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "white";
+  ctx.beginPath();
+  for (let i = 0; i < stars.length; i++) {
+    const s = stars[i];
+    ctx.moveTo(s.x, s.y);
+    ctx.arc(s.x, s.y, s.r, 0, Math.PI * 2, true);
+  }
+  ctx.fill();
+  update();
 }
 
-function updateStars() {
-    stars.forEach(star => {
-        star.y += star.velocity;
-        if (star.y > canvas.height) {
-            star.y = 0;
-            star.x = Math.random() * canvas.width;
-        }
-    });
+function update() {
+  for (let i = 0; i < stars.length; i++) {
+    stars[i].y += stars[i].d;
+    if (stars[i].y > canvas.height) {
+      stars[i].y = 0;
+      stars[i].x = Math.random() * canvas.width;
+    }
+  }
 }
 
-drawStars();
-
-window.addEventListener('resize', () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-});
+setInterval(drawStars, 33);
